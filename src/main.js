168167072d1b02d8d1d658bfb25b25dd440319d3
Richard Lock <lock.j.h@gmail.com>
2017-06-17 10:21:24 -0500

    Webpack Tutorial by Joe Zimmerman
    Followed Joe Zimmerman's Webpack tutorial on Smashing. Added my own comments and package versions

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
+//ES2015
 import { map } from 'lodash';
+// import template from './numberlist.hbs';
 
 let numbers = map([1,2,3,4,5,6], n => n*n);
 
+//set timeout then lazyload in the Hnadlebars template and output the list to the screen
 setTimeout( () => {
     require(['./numberlist.hbs'], template => {
         document.getElementById("app-container").innerHTML = template({numbers});
     })
 }, 2000);
+
+
+
+
+
+
+
+//Regular JS
+// var map = require('lodash/map');
+
+// function square(n) {
+//     return n*n;
+// }
+
+// console.log(map([1,2,3,4,5,6], square));
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,54 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
-var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+//import plugins
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+
 
 module.exports = {
     entry: {
-        vendor: ['babel-polyfill', 'lodash'],
+        //allow chunking of common vendor files for caching purposes
+        vendor:['babel-polyfill', 'lodash'],
         main: './src/main.js'
     },
     output: {
-        path: './dist',
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
             {
-                test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/,
-                query: { plugins: ['transform-runtime'], presets: ['es2015'] }
+                test: /\.jsx?$/,
+                //loaders are read from right to left
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    plugins: ['transform-runtime'],
+                    presets: ['es2015']
+                }
             },
-            { test: /\.hbs$/, loader: 'handlebars' }
+            {
+                test: /\.hbs$/,
+                loader: 'handlebars-loader'
+            }
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Intro to Webpack',
+        new HtmlwebpackPlugin({
+            title: 'Intro to webpack',
             template: 'src/index.html'
         }),
-        new UglifyJsPlugin({
-            beautify: false,
-            mangle: { screw_ie8 : true },
-            compress: { screw_ie8: true, warnings: false },
-            comments: false
-        }),
-        new CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
+        //not used, instead using webpack's -p and -d flags
+        // new UglifyJsPlugin({
+        //     beautify: false,
+        //     mangle: { screw_ie8 : true },
+        //     compress: { screw_ie8: true, warnings: false },
+        //     comments: false
+        // }),
+        new CommonsChunkPlugin({
+            //specifying vendor entry point as the chunk to be based off on and will be stored in the file vendor.bundle.js
+            name: "vendor",
+            filename: "vendor.bundle.js"
+        })
     ]
 };
